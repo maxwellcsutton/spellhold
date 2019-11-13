6e836b8f775964e51c9108732bbb9f202702e98f
maxwellcsutton <57697520+maxwellcsutton@users.noreply.github.com>
2019-11-13 01:40:58 -0800

    making text next to the images but poorly

--- src/getCardData.js
+++ src/getCardData.js
@@ -67,7 +67,7 @@ async function printCardData(){
     cardData.forEach((elem)=>{
         let dynamicImageParent = document.createElement("p")
         let dynamicImage = dynamicCardInfo.appendChild(dynamicImageParent)
-        dynamicImage.innerHTML = `<img id="card-image" align="left" src=${elem.image} alt=${elem.setCode}>`
+        dynamicImage.innerHTML = `<img id="card-image" src=${elem.image} alt=${elem.setCode} style="float: left">`
         let dynamicParent = document.createElement("p")
         let dynamicInfoChild = dynamicCardInfo.appendChild(dynamicParent)
         dynamicInfoChild.innerHTML = 
