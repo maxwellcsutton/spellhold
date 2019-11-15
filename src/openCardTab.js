4ecd84a3f569871103e8f5c00b3fc956af7c9aff
maxwellcsutton <57697520+maxwellcsutton@users.noreply.github.com>
2019-11-14 20:53:47 -0800

    more edgecase management and I made some really ugly tabs

---
+++ src/openCardTab.js
@@ -0,0 +1,13 @@
+function openCardTab(event, code) {
+    let i
+    let tabcontent = document.getElementsByClassName("tabcontent");
+    let tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    code.style.display = "block";
+    event.currentTarget.className += " active";
+  }
