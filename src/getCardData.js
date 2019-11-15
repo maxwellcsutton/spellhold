aa76e0708ce097499b8d7a5fe6f72cda0d5bb709
maxwellcsutton <57697520+maxwellcsutton@users.noreply.github.com>
2019-11-14 19:38:30 -0800

    improving handling for planeswalkers, creatures, double sided cards, and cards with commas in the name

--- src/getCardData.js
+++ src/getCardData.js
@@ -1,10 +1,14 @@
 // for live
 
 let rawCardName = localStorage["submission"]
-// for testing in the command line
+// for testing in the command line - (npm i node-fetch)
 // const fetch = require("node-fetch")
-// let rawCardName = "force of will"
+// let rawCardName = "Jace, Vryn's Prodigy // Jace, Telepath Unbound"
 let cardName = rawCardName.split(" ").join("+")
+let specialCharacterCheck = cardName.includes(",")
+if (specialCharacterCheck){
+    cardName = cardName.split(",").join("")
+}
 let cardData = []
 
 function createCardDataObjectforEachSet(index){
@@ -12,15 +16,14 @@ function createCardDataObjectforEachSet(index){
     let foilPrice = index.prices.usd_foil
     let buyPrice = Math.ceil(price * 60) / 100
     let foilBuyPrice = Math.ceil(foilPrice * 60) / 100
+    let isDoubleSided = index.card_faces
+    let isCreature = index.power
+    let isPlaneswalker = index.loyalty
     let obj = new Object()
     obj.name = index.name
     obj.setCode = index.set
     obj.setName = index.set_name
     obj.rarity = index.rarity
-    obj.color = index.colors
-    obj.cost = index.mana_cost
-    obj.type = index.type_line
-    obj.text = index.oracle_text
     if (price){
     obj.price = "$"+index.prices.usd
     obj.projectedBuyPrice = "$"+buyPrice
@@ -36,8 +39,45 @@ function createCardDataObjectforEachSet(index){
         obj.projectedFoilBuyPrice = "Printings in this set are only available in non-foil."
     }
     obj.link = index.purchase_uris.tcgplayer
-    obj.image = index.image_uris.small
+    if (!isDoubleSided){
+        obj.color = index.colors
+        obj.cost = index.mana_cost
+        obj.type = index.type_line
+        obj.text = index.oracle_text
+        obj.image = index.image_uris.small
+        if (isCreature){
+            obj.power = index.power
+            obj.toughness = index.toughness
+        }
+        if (isPlaneswalker){
+            obj.loyalty = isPlaneswalker
+        }
+    } else if (isDoubleSided){
+        let faces = []
+        obj.cardFaces = faces
+        isDoubleSided.forEach((side)=>{
+            let isDoubleSidedCreature = side.power
+            let isDoubleSidedPlaneswalker = side.loyalty
+            let doubleSided = new Object()
+            doubleSided.name = side.name
+            doubleSided.color = side.colors
+            doubleSided.cost = side.mana_cost
+            doubleSided.type = side.type_line
+            doubleSided.text = side.oracle_text
+            doubleSided.image = side.image_uris.small
+            // if you're reading this and are unfamiliar with magic, look up Jace, Vryn's Prodigy to see how this can get hairy
+            if (isDoubleSidedCreature){
+                doubleSided.power = side.power
+                doubleSided.toughness = side.toughness
+            }
+            if (isDoubleSidedPlaneswalker){
+                doubleSided.loyalty = side.loyalty
+            }
+            faces.push(doubleSided)
+        })
+    }
     if (price || foilPrice){
+        //this excludes all online only printings
         cardData.push(obj)
     }
     return 
@@ -48,23 +88,71 @@ async function getCardData(){
     let json = await response.json()
     let allPrintingsObj = json.data
     allPrintingsObj.forEach(createCardDataObjectforEachSet)
-    //console.log(cardData)
+    // console.log(cardData)
 }
 
-//getCardData()
+// getCardData()
 
 async function printCardData(){
     await getCardData()
+    //this will display all the info that doesn't change per set at the top, but I might
+    //move all the card data together once I add tabs for each set
     let staticCardInfo = document.getElementById('static-card-info');
-    let staticParent = document.createElement("p")
-    let staticChild = staticCardInfo.appendChild(staticParent)
-    staticChild.innerHTML = 
-        `<b>Color: </b>${cardData[0].color}<br>
-        <b>Mana Cost: </b>${cardData[0].cost}<br>
-        <b>Type: </b>${cardData[0].type}<br>
-        <b>Rules Text: </b>${cardData[0].text}<br>`
+    let staticParent1 = document.createElement("p")
+    let staticChild1 = staticCardInfo.appendChild(staticParent1)
+    let isCreature = cardData[0].power
+    let isPlaneswalker = cardData[0].loyalty
+    if (!cardData[0].cardFaces){
+        let staticCardDataHTML =
+            `<b>Color: </b>${cardData[0].color}<br>
+            <b>Mana Cost: </b>${cardData[0].cost}<br>
+            <b>Type: </b>${cardData[0].type}<br>
+            <b>Rules Text: </b>${cardData[0].text}<br>`
+         if (!cardData[0].cardFaces){
+            if (!isCreature && !isPlaneswalker){
+                staticChild1.innerHTML = staticCardDataHTML
+            } else if (isCreature){
+                staticChild1.innerHTML = staticCardDataHTML + 
+                    `<b>Power: </b>${cardData[0].power}<br>
+                    <b>Toughness: </b>${cardData[0].toughness}<br>`
+            } else if (isPlaneswalker){
+                staticChild1.innerHTML = staticCardDataHTML + 
+                    `<b>Loyalty: </b>${cardData[0].loyalty}<br>`
+            }
+        }
+    } else if (cardData[0].cardFaces) {
+        let staticParent2 = document.createElement("p")
+        let staticChild2 = staticCardInfo.appendChild(staticParent2)
+        let eachFaceHTML = []
+        let eachFace = cardData[0].cardFaces
+        eachFace.forEach((face)=>{
+            let staticCardDataHTML = 
+            `<b>Name: </b>${face.name}<br>
+            <b>Color: </b>${face.color}<br>
+            <b>Mana Cost: </b>${face.cost}<br>
+            <b>Type: </b>${face.type}<br>
+            <b>Rules Text: </b>${face.text}<br>`
+            let isCreature = face.power
+            let isPlaneswalker = face.loyalty
+            if (!isCreature && !isPlaneswalker){
+                eachFaceHTML.push(staticCardDataHTML)
+            } else if (isCreature){
+                staticCardDataHTML = staticCardDataHTML + 
+                    `<b>Power: </b>${face.power}<br>
+                    <b>Toughness: </b>${face.toughness}<br>`
+                eachFaceHTML.push(staticCardDataHTML)
+            } else if (isPlaneswalker) {
+                staticCardDataHTML = staticCardDataHTML +
+                    `<b>Loyalty: </b>${face.loyalty}<br>`
+                eachFaceHTML.push(staticCardDataHTML)
+            }
+        })
+        staticChild1.innerHTML = eachFaceHTML[0]
+        staticChild2.innerHTML = eachFaceHTML[1]
+        }
     let dynamicCardInfo = document.getElementById('dynamic-card-info');
     cardData.forEach((elem)=>{
+        //this will display all info that changes per set
         let dynamicImageParent = document.createElement("p")
         let dynamicImage = dynamicCardInfo.appendChild(dynamicImageParent)
         dynamicImage.innerHTML = `<img id="card-image" src=${elem.image} alt=${elem.setCode}>`
